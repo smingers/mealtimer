39aa124444d3dc430f63a7a3cdc77034d132e41c
Jordan <jordanberman@gmail.com>
2014-02-16 22:10:09 -0500

    removed some unnecessary console.log-based testing from home.js

--- html/js/home.js
+++ html/js/home.js
@@ -10,10 +10,6 @@ $(document).ready(function () {
         var id = recipes.id;
         var recipeDiv = "<div class=\"col-xs-6 col-md-4 recipe\"><a class=\"thumbnail\" href=\"..\\html\\recipe.html?id=" + id + "\"><img src=\"" + image + "\" alt=\"\"></a><h4 class\"title\">" + title + "</h4><p class=\"description\">" + description + "</p></div>";
         $('.recipe-grid').append(recipeDiv);
-        console.log(recipes.id);
-        console.log(recipes.title);
-        console.log(recipes.description);
-        console.log(recipes.bgImage);
     };
    
    // get recipe from JSON array of objects; currently hard coding the ID number
