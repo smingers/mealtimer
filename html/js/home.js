280477d52e2ed528e2c15dfa7ff056b9157e13cc
Jordan <jordanberman@gmail.com>
2014-02-18 23:19:38 -0500

    very cheap tricks to fix alignment issues on homepage

--- html/js/home.js
+++ html/js/home.js
@@ -3,9 +3,12 @@ $(document).ready(function () {
     var buildRecipeDiv = function (recipes) {
         var image = recipes.bgImage;
         var title = recipes.title;
+        if (recipes.title.length > 40) {
+            title = recipes.title.substring(0, 40) + "...";
+        }
         var description = recipes.description;
-        if (recipes.description.length > 150) {
-            description = recipes.description.substring(0, 150) + "...";
+        if (recipes.description.length > 80) {
+            description = recipes.description.substring(0, 80) + "...";
         }
         var id = recipes.id;
         var recipeDiv = "<div class=\"col-xs-6 col-md-4 recipe\"><a class=\"thumbnail\" href=\"..\\html\\recipe.html?id=" + id + "\"><img src=\"" + image + "\" alt=\"\"></a><h4 class\"title\">" + title + "</h4><p class=\"description\">" + description + "</p></div>";
