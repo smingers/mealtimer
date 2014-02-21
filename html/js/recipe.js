b1049d8266d7301484b110a38316d00f23921471
Jordan <jordanberman@gmail.com>
2014-02-20 23:48:29 -0500

    fixed position of popover

--- html/js/recipe.js
+++ html/js/recipe.js
@@ -400,6 +400,7 @@ $(document).ready(function () {
     });
     
     $('.more').popover({
+        position: 'fixed',
         placement: 'bottom',
         html: 'true',
         content : '<div class="btn-group-vertical"><button type="button" class="btn btn-default add"><span class="glyphicon glyphicon-plus"></span> Add a minute</button><button type="button" class="btn btn-default subtract"><span class="glyphicon glyphicon-minus"></span> Subtract a minute</button><button type="button" class="btn btn-default reset"><span class="glyphicon glyphicon-repeat"></span> Reset time</button></div>'
