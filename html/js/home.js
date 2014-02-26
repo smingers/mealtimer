b75fb2e2eeda58a2f93dd4da42c0223e8a2ba46d
Jordan <jordanberman@gmail.com>
2014-02-25 22:52:03 -0500

    scaled-back ambitions on home grid, everything is the same height, no description

--- html/js/home.js
+++ html/js/home.js
@@ -3,15 +3,15 @@ $(document).ready(function () {
     var buildRecipeDiv = function (recipes) {
         var image = recipes.bgImage;
         var title = recipes.title;
-        if (recipes.title.length > 40) {
-            title = recipes.title.substring(0, 40) + "...";
+        if (recipes.title.length > 60) {
+            title = recipes.title.substring(0, 60) + "...";
         }
         var description = recipes.description;
         if (recipes.description.length > 80) {
             description = recipes.description.substring(0, 80) + "...";
         }
         var id = recipes.id;
-        var recipeDiv = "<div class=\"col-xs-6 col-md-4 recipe\"><a class=\"thumbnail\" href=\"..\\html\\recipe.html?id=" + id + "\"><img src=\"" + image + "\" alt=\"\"></a><h4 class=\"title\">" + title + "</h4><p class=\"description\">" + description + "</p></div>";
+        var recipeDiv = "<div class=\"col-xs-6 col-md-4 recipe\"><a class=\"thumbnail\" href=\"..\\html\\recipe.html?id=" + id + "\"><img src=\"" + image + "\" alt=\"\"><div class=\"title-description\"><h4 class=\"title\">" + title + "</h4></div></div></a>";
         $('.recipe-grid').append(recipeDiv);
     };
     
