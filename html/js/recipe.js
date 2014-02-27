cf466ae8d813826cfd9b38f5aedbe67d6c1e4daa
Jordan <jordanberman@gmail.com>
2014-02-27 13:58:16 -0500

    add if/else logic for passive panels; play button

--- html/js/recipe.js
+++ html/js/recipe.js
@@ -66,6 +66,7 @@ $(document).ready(function () {
         
         var totalTime = 0;
         var numNullSteps = 0; // for inserting null-time steps into the progress bar
+        var passiveButtons = "<button type=\"button\" class=\"btn btn-default small play\"><span class=\"glyphicon glyphicon-play\"></span></button>";
         
         // step panels
         for (var i = 0, length = recipe.steps.length; i < length; i++) {
@@ -73,7 +74,13 @@ $(document).ready(function () {
             var stepNum = recipe.steps[i].ordinal;
             var stepTime = textTime(convertMS(recipe.steps[i].time));
             var stepText = recipe.steps[i].text;
-            $('.steps').append("<div class=\"panel panel-default\" id=\"" + stepNum + "\"><div class=\"panel-heading\"><h3 class=\"panel-title \">Step <span class=\"step-number\">" + stepNum + "</span> <small><span class=\"step-time\">" + stepTime + "</span><span class=\"elapsed-time\"></span></small></h3></div><div class=\"panel-body\">" + stepText + "</div></div>");
+            
+            // IN PROGRESS
+            if (recipe.steps[i].passive) {
+                $('.steps').append("<div class=\"panel panel-default passive\" id=\"" + stepNum + "\"><div class=\"panel-heading\"><h3 class=\"panel-title \">Step <span class=\"step-number\">" + stepNum + "</span> <small><span class=\"step-time\">" + stepTime + "</span>" + passiveButtons + "<span class=\"elapsed-time\"></span></small></h3></div><div class=\"panel-body\">" + stepText + "</div></div>");
+            } else {
+                $('.steps').append("<div class=\"panel panel-default\" id=\"" + stepNum + "\"><div class=\"panel-heading\"><h3 class=\"panel-title \">Step <span class=\"step-number\">" + stepNum + "</span> <small><span class=\"step-time\">" + stepTime + "</span><span class=\"elapsed-time\"></span></small></h3></div><div class=\"panel-body\">" + stepText + "</div></div>");
+            }
             
             recipeStepTimes.push(recipe.steps[i].time);
             totalTime += recipe.steps[i].time;
