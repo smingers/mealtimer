0be7973b827bfe6af5e219d13f947dd5a8441cbf
smingers <prplemnkydshwshr@yahoo.com>
2014-03-20 13:57:33 -0400

    rollback

--- web.js
+++ web.js
@@ -1,10 +1,14 @@
-var http = require("http");
+var express = require("express");
+var logfmt = require("logfmt");
+var app = express();
 
-http.createServer(function (request, response) {
-	console.log("request received!");
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Hello, World");
-	response.end();
-}).listen(8888);
+app.use(logfmt.requestLogger());
 
-console.log("Server has started.");
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
