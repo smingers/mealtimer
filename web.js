49aa6b982f220515a2758bc461f56e3a8960a6ab
smingers <prplemnkydshwshr@yahoo.com>
2014-03-24 15:43:34 -0400

    test different web.js configuration

--- web.js
+++ web.js
@@ -1,14 +1,11 @@
-var express = require("express");
-var logfmt = require("logfmt");
-var app = express();
+var http = require('http');
+var fs = require('fs');
 
-app.use(logfmt.requestLogger());
+http.createServer(function(request, response) {
+  response.writeHead(200);
 
-app.get('/', function(req, res) {
-  res.send('coming soon...');
-});
-
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+  fs.readFile('home.html', function(err, contents) {
+    response.write(contents);
+    response.end();
+  });
+}).listen(8080);
