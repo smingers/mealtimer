2dcf148e702043802b04ea139b027d71b91e0a2d
smingers <prplemnkydshwshr@yahoo.com>
2014-03-24 20:37:37 -0400

    test different web.js configuration - take 5

--- web.js
+++ web.js
@@ -1,17 +1,12 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
-var http = require('http');
-var fs = require('fs');
 
 app.use(logfmt.requestLogger());
 
-app.get('/', function(req, res) {
- 	res.send('coming soon...');
+app.get('/', function(request, response) {
+ 	response.sendfile(__dirname + '/home.html');
 
- 	fs.readFile('/html/home.html', function(err, contents) {
-    response.write(contents);
-    response.end();
   });
 
 });
